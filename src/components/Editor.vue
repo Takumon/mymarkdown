13cc5c61a11e01c68ad5735d3c492e2a368ceb25
takumon <inouetakumon@gmail.com>
2018-05-02 07:39:21 +0900

    エディタ機能を追加

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,26 +3,46 @@
     <h1>エディター画面</h1>
     <span>{{user.displayName}}</span>
     <button @click="logout">ログアウト</button>
+    <div class="editorWrapper">
+      <textarea class="markdown" v-model="markdown"></textarea>
+      <div class="preview" v-html="preview()"></div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'editor',
-    // 親コンポーネントから渡されるオブジェクト
-    props: ['user'],
-    data () {
-      return {
-      }
-    },
-    methods: {
-      logout: function() {
-        firebase.auth().signOut();
-      }
+import marked from 'marked';
+
+export default {
+  name: 'editor',
+  // 親コンポーネントから渡されるオブジェクト
+  props: ['user'],
+  data () {
+    return {
+      markdown: '',
     }
+  },
+  methods: {
+    logout: function() {
+      firebase.auth().signOut();
+    },
+    preview: function() {
+      return marked(this.markdown)
+    },
   }
+}
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.editorWrapper {
+  display: flex;
+}
+.markdown {
+  width: 50%;
+  height: 500px;
+}
+.preview {
+  width: 50%;
+  text-align: left;
+}
 </style>
