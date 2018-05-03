72c536068fe3de0de086356dd4d7fb57f129633e
takumon <inouetakumon@gmail.com>
2018-05-03 21:10:06 +0900

    ログイン方法各種実装

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,28 @@
 <template>
   <div id="home">
     <h1>{{msg}}</h1>
-    <button @click="googleLogin">Google アカウントでログイン</button>
+    <div class="login-button-area">
+      <div class="btn-wrapper">
+        <md-button
+          class="md-raised md-primary"
+          @click="loginWithGoogle"><i class="btn-icon fab fa-google"></i> Google アカウントでログイン</md-button>
+      </div>
+      <div class="btn-wrapper">
+        <md-button
+          class="md-raised md-primary"
+          @click="loginWithFacebook"><i class="btn-icon fab fa-facebook"></i> Facebook アカウントでログイン</md-button>
+      </div>
+      <div class="btn-wrapper">
+        <md-button
+          class="md-raised md-primary"
+          @click="loginWithTwitter"><i class="btn-icon fab fa-twitter"></i> Twitter アカウントでログイン</md-button>
+      </div>
+      <div class="btn-wrapper">
+        <md-button
+          class="md-raised md-primary"
+          @click="loginWithGithub"><i class="btn-icon fab fa-github"></i> GitHub アカウントでログイン</md-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,10 +34,40 @@
         msg: 'Welcome to MyMarkdown'
       }
     },
+    // 認証失敗時の処理
     methods: {
-      googleLogin: function() {
+      loginWithGoogle: function() {
         firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
-      }
+      },
+      loginWithFacebook: function() {
+        firebase.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider());
+      },
+      loginWithTwitter: function() {
+        firebase.auth().signInWithPopup(new firebase.auth.TwitterAuthProvider());
+      },
+      loginWithGithub: function() {
+        firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider());
+      },
     }
   }
 </script>
+
+<style lang="scss" scoped>
+.login-button-area {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+
+  .btn-wrapper {
+    width: 360px;
+
+    button {
+      width: 100%;
+
+      .btn-icon {
+        margin-right: 10px;
+      }
+    }
+  }
+}
+</style>
