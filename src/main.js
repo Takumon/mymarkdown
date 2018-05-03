52f4d4c965a8cc0cd0a3342c0bfd31a93a683a96
takumon <inouetakumon@gmail.com>
2018-05-03 21:09:14 +0900

    vue material導入

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import './scss/style.scss'
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import App from './App.vue'
 
 Vue.config.devtools = true
+Vue.use(VueMaterial)
 
 new Vue({
   el: '#app',
