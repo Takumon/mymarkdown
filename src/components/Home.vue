5cbb5d0291d7585226474e20a3ed67fa1dee77db
takumon <inouetakumon@gmail.com>
2018-05-03 22:15:04 +0900

    ログイン画面作成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="home">
-    <h1>{{msg}}</h1>
-    <div class="login-button-area">
-      <div class="btn-wrapper">
+    <h1 class="app-title">{{app.title}}</h1>
+    <p class="app-discription">{{app.discription}}</p>
+    <md-content class="content md-elevation-6">
+      <div class="content-discription">Login with ...</div>
+      <div class="login-button-area">
         <md-button
           class="md-raised md-primary"
-          @click="loginWithGoogle"><i class="btn-icon fab fa-google"></i> Google アカウントでログイン</md-button>
-      </div>
-      <div class="btn-wrapper">
+          @click="loginWithGoogle"><i class="btn-icon fab fa-google"></i> Google</md-button>
         <md-button
           class="md-raised md-primary"
-          @click="loginWithFacebook"><i class="btn-icon fab fa-facebook"></i> Facebook アカウントでログイン</md-button>
-      </div>
-      <div class="btn-wrapper">
+          @click="loginWithFacebook"><i class="btn-icon fab fa-facebook"></i> Facebook</md-button>
         <md-button
           class="md-raised md-primary"
-          @click="loginWithTwitter"><i class="btn-icon fab fa-twitter"></i> Twitter アカウントでログイン</md-button>
-      </div>
-      <div class="btn-wrapper">
+          @click="loginWithTwitter"><i class="btn-icon fab fa-twitter"></i> Twitter</md-button>
         <md-button
           class="md-raised md-primary"
-          @click="loginWithGithub"><i class="btn-icon fab fa-github"></i> GitHub アカウントでログイン</md-button>
+          @click="loginWithGithub"><i class="btn-icon fab fa-github"></i> GitHub</md-button>
       </div>
-    </div>
+    </md-content>
   </div>
 </template>
 
@@ -31,8 +27,11 @@
     name: 'home',
     data () {
       return {
-        msg: 'Welcome to MyMarkdown'
-      }
+        app: {
+          title: 'Markdown Mome',
+          discription: 'Online markdown memo',
+        }
+      };
     },
     // 認証失敗時の処理
     methods: {
@@ -53,20 +52,47 @@
 </script>
 
 <style lang="scss" scoped>
+.app-title {
+  font-size: 64px;
+  margin-bottom: 48px;
+}
+
+.app-discription {
+    margin-bottom: 64px;
+    color: #000882;
+}
+
+.content {
+  width: 480px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 36px 0;
+  border-radius: 2px;
+}
+.content-discription {
+  text-align: left;
+  font-size: 36px;
+  text-indent: 36px;
+  color: #0c2ad0;
+  font-weight: 100;
+  border-left: 6px solid;
+  height: 48px;
+  line-height: 48px;
+    text-transform: uppercase;
+}
 .login-button-area {
+  margin-top: 24px;
+  margin-bottom: 24px;
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
 
-  .btn-wrapper {
+  button.md-raised {
     width: 360px;
+    margin: 6px;
 
-    button {
-      width: 100%;
-
-      .btn-icon {
-        margin-right: 10px;
-      }
+    .btn-icon {
+      margin-right: 10px;
     }
   }
 }
