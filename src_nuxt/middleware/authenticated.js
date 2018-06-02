7147878c38b0565273c15c2fd8b9be7291dc718a
takumon <inouetakumon@gmail.com>
2018-06-02 16:43:45 +0900

    ルートアクセス時はログイン画面にリダイレクトするようにした

--- src_nuxt/middleware/authenticated.js
+++ src_nuxt/middleware/authenticated.js
@@ -4,10 +4,14 @@ import { auth } from '~/plugins/auth'
 export default function ({ route, store, redirect }) {
 
   return new Promise((resolve, reject) => {
+    // ルートアクセス時はログイン画面にリダイレクト
+    if (route.path === "/") {
+      redirect('/login')
+      return resolve()
+    }
+
     // firebaseで認証管理
     auth().then(user => {
-      console.log('user')
-      console.log(user)
       if (user) {
         store.dispatch('setLoginUser', user).then(() => {
           resolve()
