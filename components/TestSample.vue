802c6db67ca7b68d7f607a744d23cd109c642fb2
takumon <inouetakumon@gmail.com>
2018-06-07 08:43:40 +0900

    vuexを使ったテストを追加、スナップショット更新

--- components/TestSample.vue
+++ components/TestSample.vue
@@ -1,19 +1,37 @@
 <template>
+<div>
   <ul>
     <li v-for="(item, index) in list" :key="index">
       {{ item }}
     </li>
   </ul>
+  <button id="toggleLoading" @click="toggle">{{showLoading}}</button>
+</div>
 </template>
 
 <script>
-  export default {
-    name: 'testSample',
-    props: {
-      list: {
-        type: Array,
-        required: true,
-      },
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'testSample',
+  props: {
+    list: {
+      type: Array,
+      required: true,
     },
-  }
+  },
+  computed: {
+    ...mapState([
+      'showLoading'
+    ]),
+  },
+  methods: {
+    ...mapActions([
+      'setShowLoading',
+    ]),
+    toggle() {
+      this.setShowLoading(! this.showLoading)
+    }
+  },
+}
 </script>
