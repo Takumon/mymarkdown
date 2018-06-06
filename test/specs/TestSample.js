9b329104e6d322bcdca33b7cbc07361b0ed83e37
takumon <inouetakumon@gmail.com>
2018-06-07 08:43:52 +0900

    vuexを使ったテストを追加

--- test/specs/TestSample.js
+++ test/specs/TestSample.js
@@ -1,14 +1,42 @@
 import test from 'ava'
-import { mount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import TestSample from '../../components/TestSample.vue'
 
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
 let wrapper
+let store
 
 const list = ['Coffee', 'Tea', 'Snicker', 'Mars']
 
 test.beforeEach(() => {
-  wrapper = mount(TestSample, {
+  store = new Vuex.Store({
+    state: {
+      showLoading: false,
+    },
+    actions: {
+      setShowLoading ({ commit }, isShow) {
+        return new Promise((resolve, reject) => {
+          commit( 'setShowLoading', { isShow })
+          resolve()
+        })
+      },
+    },
+    mutations: {
+      setShowLoading (state, payload) {
+        state.showLoading = payload.isShow
+      },
+    }
+  })
+
+  wrapper = shallowMount(TestSample, {
     propsData: { list },
+    store,
+    localVue
   })
 })
 
@@ -16,10 +44,21 @@ test('TestSample.vue スナップショット', (t) => {
   t.snapshot({ html: wrapper.html() })
 })
 
-test('TestSample.vue リストを表示', (t) => {
+test('TestSample.vue 初期表示 リストが表示される', (t) => {
   const $items = wrapper.findAll('li').wrappers
 
   $items.forEach(($item, index) => {
     t.is($item.text(), list[index])
   })
+})
+
+test('TestSample.vue 初期表示 ボタンがfalseで表示される', (t) => {
+  const $button = wrapper.find('#toggleLoading')
+  t.is($button.text(), 'false')
+})
+
+test('TestSample.vue ボタンクリック後 ボタンがtrueで表示される', (t) => {
+  const $button = wrapper.find('#toggleLoading')
+  $button.trigger('click')
+  t.is($button.text(), 'true')
 })
